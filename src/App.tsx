c3759d7d8ebd81deb5a7d78f155053c6d817987f
adamhinton <adam.d.hinton@gmail.com>
2022-09-13 16:14:19 -0400

    import pokemonData to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./App.css";
+import pokemonData from "./data";
 
 function App() {
+  console.log("pokemonData:", pokemonData);
   return (
     <div className="App">
       <h1>Adam Hinton</h1>
