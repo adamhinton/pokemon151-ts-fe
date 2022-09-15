3b36ea3a5f8eba82999e2b5ced80c9c0cb906123
adamhinton <adam.d.hinton@gmail.com>
2022-09-15 17:24:47 -0400

    load HTML (no CSS yet) in to SP.tsx, it seems to work

--- src/components/SinglePokemon.tsx
+++ src/components/SinglePokemon.tsx
@@ -12,7 +12,49 @@ const SinglePokemon = (props: Props): ReactElement => {
   const { name, type, dexEntry, height, weight, id, num } = poke;
   console.log("poke, isShiny:", poke, isShiny);
 
-  return <div>SinglePokemon</div>;
+  return (
+    <article className="pokemon-card" data-testid="pokemon-card">
+      <h2 className="poke-name card-item">
+        {name} <span className="dex-id">#{id}</span>
+      </h2>
+
+      {/* Pokemon's type info */}
+      {type[1] ? (
+        <h3 className="poke-type card-item">
+          {type[0]} / {type[1]}{" "}
+        </h3>
+      ) : (
+        <h3 className="poke-type card-item">{type}</h3>
+      )}
+
+      <h4 className="height-weight card-item">
+        Height: {height} <br />
+        Weight: {weight}
+      </h4>
+
+      {/* image of each pokemon */}
+      <figure className="img-container card-item">
+        {/* Displays either the shiny or normal form of the pokemon based on user preference */}
+        {isShiny ? (
+          <img
+            className="poke-img"
+            data-testid="shiny"
+            src={`https://www.serebii.net/Shiny/SWSH/${num}.png`}
+            alt={name}
+          />
+        ) : (
+          <img
+            className="poke-img"
+            data-testid="not-shiny"
+            src={`https://www.serebii.net/swordshield/pokemon/${num}.png`}
+            alt={name}
+          />
+        )}
+      </figure>
+
+      <blockquote className="card-item">{dexEntry}</blockquote>
+    </article>
+  );
 };
 
 export default SinglePokemon;
