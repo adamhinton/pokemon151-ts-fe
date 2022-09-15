25598225a80cc3bae7c787d45cec17ae245da5d2
adamhinton <adam.d.hinton@gmail.com>
2022-09-15 17:10:05 -0400

    define Props in SinglePokemon

--- src/components/SinglePokemon.tsx
+++ src/components/SinglePokemon.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-interface Props {}
+interface Props {
+  // Not doing type definition for poke because that is typechecked elsewhere in the code - no need to repeat here
+  poke: Object;
+  // isShiny will be added later
+  isShiny: Boolean;
+}
 
 const SinglePokemon = <Props,>(props: Props) => {
   return <div>SinglePokemon</div>;
