abef2cc5eda9a241cf5fa8a668a1b788f5433d4d
adamhinton <adam.d.hinton@gmail.com>
2022-09-16 16:52:33 -0400

    [2] Displays correct Pokemon info on screen"

--- src/jest tests/SinglePokemon.test.tsx
+++ src/jest tests/SinglePokemon.test.tsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SinglePokemon from "../components/SinglePokemon";
 import pokemonData, { PokemonData } from "../data";
 
@@ -18,4 +18,20 @@ jest.mock("../data", (): PokemonData => {
 
 test("[1] renders without errors", () => {
   render(<SinglePokemon poke={pokemonData[0]} isShiny={true} />);
+});
+
+test("[2] Displays correct Pokemon info on screen", () => {
+  render(<SinglePokemon poke={pokemonData[0]} isShiny={true} />);
+
+  const name = screen.queryByText(/testname123/i);
+  const type = screen.queryByText(/TestType1/i);
+  const dexEntry = screen.queryByText(/This is a test dex entry/i);
+  const height = screen.queryByText(/.0001m/i);
+  const weight = screen.queryByText(/0.0001kg/i);
+
+  expect(name).toBeVisible();
+  expect(type).toBeVisible();
+  expect(dexEntry).toBeVisible();
+  expect(height).toBeVisible();
+  expect(weight).toBeVisible();
 });
