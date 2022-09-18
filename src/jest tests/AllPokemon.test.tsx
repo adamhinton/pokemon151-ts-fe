5b3a4dac37f7370c55d02de854696f1a572d9f15
adamhinton <adam.d.hinton@gmail.com>
2022-09-17 21:57:26 -0400

    mock pokemonData in AP.test

--- src/jest tests/AllPokemon.test.tsx
+++ src/jest tests/AllPokemon.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import AllPokemon from "../components/AllPokemon";
-import pokemonData, { PokemonData } from "../data";
+import pokemonData from "../data";
 import renderer from "react-test-renderer";
 
 jest.mock("../components/SinglePokemon", () => () => {
@@ -11,27 +11,8 @@ jest.mock("../components/SinglePokemon", () => () => {
   );
 });
 
-jest.mock("../data", (): PokemonData => {
-  return [
-    {
-      id: 1,
-      num: "001",
-      name: "Testname123",
-      type: ["TestType1", "TestType2"],
-      height: "0.0001m",
-      weight: "0.0001kg",
-      dexEntry: "This is a test dex entry",
-    },
-    {
-      id: 2,
-      num: "002",
-      name: "Testname2",
-      type: ["TestType2", "TestType3"],
-      height: "0.0001m",
-      weight: "0.0001kg",
-      dexEntry: "This is a test dex entry",
-    },
-  ];
+jest.mock("../data", () => {
+  return [{ name: "Test123" }, { name: "Test345" }];
 });
 
 test("[1] Renders without errors", () => {
