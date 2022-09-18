36ac3bf5354bd8540494d0d1fa1368bc99f1afc8
adamhinton <adam.d.hinton@gmail.com>
2022-09-18 15:31:54 -0400

    instate global styling

---
+++ src/globalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  max-width: 100%;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #3c9893;
+  font-family: "Ubuntu", sans-serif;
+  margin: 0;
+  text-align: center;
+  /* Below is darkmode stuff */
+  --button-text: #000;
+  --button-bg: #fff;
+}
+`;
+
+export default GlobalStyle;
