e3f3d7bbf5c4d918738924acedbfe772e17f26de
adamhinton <adam.d.hinton@gmail.com>
2022-09-19 18:02:50 -0400

    import Header to App and put dummy Header in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import "./App.css";
 import AllPokemon from "./components/AllPokemon";
+import Header from "./components/Header";
 import pokemonData from "./data";
 import GlobalStyle from "./globalStyles";
 
@@ -8,6 +9,7 @@ const App = (): ReactElement => {
   return (
     <div className="App">
       <GlobalStyle />
+      <Header />
       <h1>Adam Hinton</h1>
       <AllPokemon isShiny={true} pokemonData={pokemonData} />
     </div>
