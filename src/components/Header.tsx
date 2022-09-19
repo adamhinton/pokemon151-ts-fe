fbf3c9f2597d9a39ab2cb324675f3aae27f5aef9
adamhinton <adam.d.hinton@gmail.com>
2022-09-19 18:06:57 -0400

    todo list

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,13 @@
+import React from "react";
+//darkmode toggle icons
+import { BsMoon, BsSun } from "react-icons/bs";
+
+// TODO:
+// - Dropdown
+// - Darkmode
+// - linksArray
+// - linkMaker
+
 interface Props {}
 
 const Header = (props: Props) => {
