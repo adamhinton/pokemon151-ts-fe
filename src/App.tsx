b97062961d1100fc97caa668fbdd862a1a01e483
adamhinton <adam.d.hinton@gmail.com>
2022-09-22 17:44:49 -0400

    wrap App in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ import Header from "./components/Header";
 import pokemonData from "./data";
 import GlobalStyle from "./globalStyles";
 import useDarkMode from "./hooks/useDarkMode";
+import { ThemeProvider } from "styled-components";
 
 const App = (): ReactElement => {
   const [isShiny, setIsShiny] = useState(false);
   const [isDarkMode] = useDarkMode();
+  const themeMode = isDarkMode ? darkTheme : lightTheme;
 
   return (
     <div className="App">
-      <GlobalStyle />
-      {/* TODO: fix the shiny stuff */}
-      <Header isShiny={isShiny} setIsShiny={setIsShiny} />
-      <h1>Adam Hinton</h1>
-      <AllPokemon pokemonData={pokemonData} isShiny={isShiny} />
+      <ThemeProvider theme={{ themeMode }}>
+        <GlobalStyle />
+        <Header isShiny={isShiny} setIsShiny={setIsShiny} />
+        <h1>Adam Hinton</h1>
+        <AllPokemon pokemonData={pokemonData} isShiny={isShiny} />
+      </ThemeProvider>
     </div>
   );
 };
 
 export default App;
+
+export const lightTheme = {};
+export const darkTheme = {};
